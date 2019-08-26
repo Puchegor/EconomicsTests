72e25063076e8f210b03894533ea679008c9ce32
Puchegor <diz@tut.by>
2019-08-27 02:01:30 +0300

    alpha 14.08.2019 hp

--- app/src/main/java/com/elmareos/economicstests/DBHelper.java
+++ app/src/main/java/com/elmareos/economicstests/DBHelper.java
@@ -1,21 +1,95 @@
 package com.elmareos.economicstests;
 
 import android.content.Context;
+import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 class DBHelper extends SQLiteOpenHelper {
+    private static String DB_PATH = "/data/data/com.elmareos.economicstests/databases/";
+    private static String DB_NAME = "ectheory.db";
+    private SQLiteDatabase myDataBase;
+    private final Context mContext;
+
     public DBHelper(Context context) {
-        super(context, "myDB", null, 1);
+        super(context, DB_NAME, null, 1);
+        this.mContext = context;
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("Create table mytable (id integer primary key autoincrement, name text)");
+        createDataBase();
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
 
+    }
+    private Boolean checkDataBase () {
+        SQLiteDatabase checkDB = null;
+        try{
+            String myPath = DB_PATH+DB_NAME;
+            checkDB = SQLiteDatabase.openDatabase(myPath,null, SQLiteDatabase.OPEN_READONLY);
+        }catch (SQLException e){}
+        if (checkDB != null) {
+            checkDB.close();
+            return true;
+        } else
+            return false;
+    }
+    public void createDataBase(){
+        boolean dbExist = checkDataBase();
+
+        if(dbExist){
+            //ничего не делать - база уже есть
+        }else{
+            //вызывая этот метод создаем пустую базу, позже она будет перезаписана
+            this.getReadableDatabase();
+
+            try {
+                copyDataBase();
+            } catch (IOException e) {
+                throw new Error("Error copying database");
+            }
+        }
+    }
+    private void copyDataBase() throws IOException {
+        //Открываем локальную БД как входящий поток
+        InputStream myInput = mContext.getAssets().open(DB_NAME);
+
+        //Путь ко вновь созданной БД
+        String outFileName = DB_PATH + DB_NAME;
+
+        //Открываем пустую базу данных как исходящий поток
+        OutputStream myOutput = new FileOutputStream(outFileName);
+
+        //перемещаем байты из входящего файла в исходящий
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = myInput.read(buffer))>0){
+            myOutput.write(buffer, 0, length);
+        }
+
+        //закрываем потоки
+        myOutput.flush();
+        myOutput.close();
+        myInput.close();
+    }
+    public void openDataBase() throws SQLException{
+        //открываем БД
+        String myPath = DB_PATH + DB_NAME;
+        myDataBase = SQLiteDatabase.openDatabase(myPath, null, SQLiteDatabase.OPEN_READONLY);
+    }
+
+    @Override
+    public synchronized void close() {
+        if(myDataBase != null)
+            myDataBase.close();
+        super.close();
     }
 }
